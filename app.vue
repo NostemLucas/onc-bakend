e33e1dc9e980f0879b1d0ecbd0405258cc9e00a5
Limberg Edgar Montes Tancara <nostemlucas@gmail.com>
2025-03-25 12:39:51 -0400

    feat: Initial Commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+
+    <UButton class="bg-amber-300">Button</UButton>
+  </div>
+</template>
+
+<style></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "@nuxt/image",
+  ],
+  css: ["~/assets/css/main.css"],
+  typescript: {
+    strict: true,
+  },
+});
